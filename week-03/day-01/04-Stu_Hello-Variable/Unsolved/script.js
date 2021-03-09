a64c1861fc81fee583dde6f0cc9fd6740b33a135
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-09 21:57:01 +0000

    resolved 04-Stu_Hello-Variable

--- week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
+++ week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,18 @@
 // Write Your JavaScript Code Here
+var name = "Kris";
+console.log("My name is " + name + ".");
+
+var pets = 3;
+console.log("I have " + pets + " pet(s).");
+
+var funFact = "I have 6 toes";
+console.log("Fun fact: " + funFact + ".");
+
+name = "Justyna";
+console.log("My name is " + name + ".");
+
+pets = 4;
+console.log("I have " + pets + " pet(s).");
+
+funFact = "I'm actually a robot";
+console.log("Fun fact: " + funFact + ".");
