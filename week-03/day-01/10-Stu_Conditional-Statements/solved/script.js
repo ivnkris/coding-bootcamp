f9271ca644fbfdc44076754c56471e42539ce624
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-10 13:07:55 +0000

    resolved 10-Stu_Conditional-Statements

---
+++ week-03/day-01/10-Stu_Conditional-Statements/solved/script.js
@@ -0,0 +1,21 @@
+// Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 50;
+
+// Write Your JavaScript Code Here
+function comparison(expr1, expr2) {
+  if (expr1 && expr2) {
+    console.log("True✅ True✅");
+  } else {
+    if (expr1 && !expr2) {
+      console.log("True✅ False❌);
+    } else {
+      if (!expr1 && expr2) {
+        console.log("False ❌ True ✅")
+      } else {
+        console.log ("False ❌ False ❌")
+      }
+    }
+  }
+}
