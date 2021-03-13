2059c51617025e8ce6be71a5cf56c1f90a4397d1
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-13 17:17:27 +0000

    resolved 16-Stu_Functions

--- week-03/day-02/16-Stu_Functions/Unsolved/script.js
+++ week-03/day-02/16-Stu_Functions/Unsolved/script.js
@@ -1,1 +1,14 @@
 // Write Your Code Below
+function compareEquality(value1, value2) {
+  if (value1 === value2) {
+    console.log("They are equal in type and value");
+  } else if (value1 == value2) {
+    console.log("They are equal in value");
+  } else {
+    console.log("The values are not equal");
+  }
+}
+
+compareEquality(1, 1);
+compareEquality("1", 1);
+compareEquality("bird", 1);
