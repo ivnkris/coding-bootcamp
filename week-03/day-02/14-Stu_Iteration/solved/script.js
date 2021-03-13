39c5c449e631c12a1876c2bfef020b73c26f4130
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-03-13 17:10:59 +0000

    added message template to 14-Stu_Iteration

--- week-03/day-02/14-Stu_Iteration/solved/script.js
+++ week-03/day-02/14-Stu_Iteration/solved/script.js
@@ -4,5 +4,6 @@ const students = ["John", "Jack", "Jay", "Jordan", "Julien"];
 console.log(students.length);
 
 for (let i = 0; i < students.length; i++) {
-  console.log("Great to see you, " + students[i]);
+  const message = `Great to see you, ${students[i]}!`;
+  console.log(message);
 }
